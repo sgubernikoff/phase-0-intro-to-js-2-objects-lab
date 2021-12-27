fbbb969fdfc78875dbef5bc92b0cb5f39a939f2a
Sam Gubernikoff <sgubernikoff@gmail.com>
2021-12-27 16:19:37 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+const employee = {
+  name: "Sam",
+  streetAddress: "275 Wickham Road",
+};
+function updateEmployeeWithKeyAndValue(employeeObject, key, value) {
+  const newEmployee = { ...employee };
+  newEmployee[key] = value;
+  return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee.streetAddress = "12 Broadway";
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  const newerEmployee = { ...employee };
+  delete newerEmployee.name;
+  return newerEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee.name;
+  return employee;
+}

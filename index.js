239b0aa5c7d2de7cfeeef71a24520a22bd31badf
Sam Gubernikoff <sgubernikoff@gmail.com>
2022-02-02 14:00:12 -0500

    commit tree 898ecf55b26991b2f1eb1794ce91ef389fdfbcd6

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
+// const employee = {
+//   name: "Sam",
+//   streetAddress: "275 Wickham Road",
+// };
+// function updateEmployeeWithKeyAndValue(employeeObject, key, value) {
+//   const newEmployee = { ...employee };
+//   newEmployee[key] = value;
+//   return newEmployee;
+// }
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+//   employee.streetAddress = "12 Broadway";
+//   return employee;
+// }
+// function deleteFromEmployeeByKey(employee, key) {
+//   const newerEmployee = { ...employee };
+//   delete newerEmployee.name;
+//   return newerEmployee;
+// }
+// function destructivelyDeleteFromEmployeeByKey(employee, key) {
+//   delete employee.name;
+//   return employee;
+// }
 const employee = {
   name: "Sam",
-  streetAddress: "275 Wickham Road",
+  streetAddress: "Mars",
 };
+
 function updateEmployeeWithKeyAndValue(employeeObject, key, value) {
-  const newEmployee = { ...employee };
-  newEmployee[key] = value;
-  return newEmployee;
+  let newObj = { ...employee };
+  newObj.streetAddress = "11 Broadway";
+  return newObj;
 }
+
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
   employee.streetAddress = "12 Broadway";
   return employee;
 }
+
 function deleteFromEmployeeByKey(employee, key) {
-  const newerEmployee = { ...employee };
-  delete newerEmployee.name;
-  return newerEmployee;
+  let newerObj = { ...employee };
+  delete newerObj.name;
+  return newerObj;
 }
+
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
   delete employee.name;
   return employee;
